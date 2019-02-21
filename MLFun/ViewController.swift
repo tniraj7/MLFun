aa4ef5062407b3a3a1fb543163a15b451c116ad3
Niraj Tiwari <tniraj7@gmail.com>
2019-02-21 14:42:59 +0900

    Initial Commit

---
+++ MLFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLFun
+//
+//  Created by Tiwari, Niraj | Nero | OSPD on 2019/02/21.
+//  Copyright © 2019 Niraj Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
